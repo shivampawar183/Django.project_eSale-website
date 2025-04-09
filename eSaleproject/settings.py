3f5d3768ca98f341d82c23927a924a67be3a2c79
Shivam Pawar <147264094+shivampawar183@users.noreply.github.com>
2025-04-10 00:59:30 +0530

    Update settings.py

--- eSaleproject/settings.py
+++ eSaleproject/settings.py
@@ -103,10 +103,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
